87c80582d000d52c3153eb090d3a51a9b1a1d5ab
Tahsin Rahman <madhacker3kxl@hotmail.com>
2015-12-08 01:12:01 -0600

    Setup code for AD5422 Testing.
    Removed folder for AD5422. Will make library later.
    Tested code for AD5422 with fixed values.
    Setup initial code for MUX

--- GLHP_Trailer.ino
+++ GLHP_Trailer.ino
@@ -1,159 +1,269 @@
-//
+/*Test code for AD5420*/
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include <SPI.h>
-#include "AD5422/AD5422.h"
 
-// Data wire is plugged into port 2 on the Arduino
-#define ONE_WIRE_BUS 2
+/***********************************************************
+**                                                        **
+**      Port Definition                                   **
+**                                                        **
+***********************************************************/
 
-// Change the precision to 12 bit
-#define TEMPERATURE_PRECISION 12
+//AD5420 LATCH signal
+#define AD5420_LATCH  10
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+/***********************************************************
+**                                                        **
+**      Definitions for DS                                **
+**                                                        **
+***********************************************************/
 
-// Pass our oneWire reference to Dallas Temperature.
-DallasTemperature sensors(&oneWire);
+//Upper temperature limit for calculation in degree C
+#define DS_Upper_Temp_Limit  60
+//Lower temperature limit for calculation in degree C
+#define DS_Lower_Temp_Limit  10
 
-// arrays to hold device addresses
-DeviceAddress
+/***********************************************************
+**                                                        **
+**      Definitions for AD5420                            **
+**                                                        **
+***********************************************************/
 
-DeviceA = {0x28, 0x20, 0x62, 0x28, 0x07, 0x00, 0x00, 0x2C},
-DeviceB = {0x28, 0x19, 0x97, 0x26, 0x07, 0x00, 0x00, 0x36},
-DeviceC = {0x28, 0x29, 0xAF, 0x27, 0x07, 0x00, 0x00, 0xEE},
-DeviceD = {0x28, 0xA2, 0xAD, 0x28, 0x07, 0x00, 0x00, 0xE5};
+//Read registers
+/*! \brief Internal address of DAC STATUS register.*/
+#define AD5420_STATUS_REG        0x00
+/*! \brief Internal address of DAC DATA register.*/
+#define AD5420_DATA_REG          0x01
+/*! \brief Internal address of DAC CONTROL register.*/
+#define AD5420_CONTROL_REG       0x02
 
-void setup(void)
-{
-  // start serial port
+//Write Registers
+/*! \brief Internal address of DAC CONTROL register.*/
+#define AD5420_CONTROL_REG_ADDR  0x55
+/*! \brief Internal address of DAC DATA register.*/
+#define AD5420_DATA_REG_ADDR     0x01
+/*! \brief Internal address of DAC RESET register.*/
+#define AD5420_RESET_REG_ADDR    0x56
+/*! \brief Internal address of NO Operation register.*/
+#define AD5420_NOP           0x00
+
+/*! \brief Internal flag of using REXT as reference resistance. 
+ * Please set this bit before or during 
+   changing #AD5420_OUTEN bit.*/
+#define AD5420_REXT              0x2000
+/*! \brief Internal flag of using RINT as reference resistance.*/
+#define AD5420_RINT              0x0000
+/*! \brief Internal flag of switching DAC on.*/
+#define AD5420_OUTEN             0x1000
+/*! \brief Internal flag of switching slew rate control on.*/
+#define AD5420_SREN              0x0010
+/*! \brief Internal flag of setting daisy-chain mode of SPI bus.*/
+#define AD5420_DCEN              0x0008
+/*! \brief Internal flag of setting 0-5 V output range.*/
+#define AD5420_0_5V_RANGE        0x0000
+/*! \brief Internal flag of setting 0-10 V output range.*/
+#define AD5420_0_10V_RANGE       0x0001
+/*! \brief Internal flag of setting 4-20 mA output range.*/
+#define AD5420_4_20I_RANGE        0x0005
+/*! \brief Internal flag of setting 0-20 mA output range.*/
+#define AD5420_0_20I_RANGE        0x0006
+/*! \brief Internal flag of setting 0-24 mA output range.*/
+#define AD5420_0_24I_RANGE        0x0007
+
+/*! \brief Internal flag of output current fault in status register.*/
+#define AD5420_IOUT_FAULT        0x0004
+/*! \brief Internal flag of slew rate being active in status register.*/
+#define AD5420_SR_ACTIVE         0x0002
+/*! \brief Internal flag of overheat fault of DAC in status register.*/
+#define AD5420_OVRHEAT_FAULT     0x0001
+
+/***********************************************************
+**                                                        **
+**      AD5420 Functions                                  **
+**                                                        **
+***********************************************************/
+void ad5420_init() {
+  //Set the Latch pin as output
+  pinMode(AD5420_LATCH, OUTPUT);
+  //Set Latch Pin as Low just in case
+  digitalWrite(AD5420_LATCH, LOW);
+
+  //Reset DAC1
+  SPI.transfer(AD5420_RESET_REG_ADDR);
+  SPI.transfer16(0x0001); //Write to reset Register
+  ad5420_latch_trig();
+
+  //Setup DAC1
+  SPI.transfer(AD5420_CONTROL_REG_ADDR);
+  SPI.transfer16(AD5420_DCEN); //Enable Daisy Chain
+  ad5420_latch_trig();
+
+  //Reset DAC2
+  SPI.transfer(AD5420_RESET_REG_ADDR);
+  SPI.transfer16(0x0001); //Write to reset Register
+  SPI.transfer(AD5420_NOP); //No Operation for DAC1
+  SPI.transfer16(0x0000);   //Send Blank
+  ad5420_latch_trig();
+
+  //Setup DAC1 and DAC2
+  //DAC2 as V out
+  SPI.transfer(AD5420_CONTROL_REG_ADDR);
+  SPI.transfer16(AD5420_OUTEN | AD5420_RINT | AD5420_0_10V_RANGE);
+  //DAC1 as 4 to 20mA
+  SPI.transfer(AD5420_CONTROL_REG_ADDR);
+  SPI.transfer16(AD5420_OUTEN | AD5420_RINT | AD5420_DCEN | AD5420_0_20I_RANGE);
+  ad5420_latch_trig();
+}
+
+//Function to trigger the latch
+void ad5420_latch_trig() {
+  digitalWrite(AD5420_LATCH, HIGH);
+  _delay_us(10);
+  digitalWrite(AD5420_LATCH, LOW);
+}
+
+void send_dac2_data(float *dac2_data) {
+  uint16_t data;
+  //Send data to DAC2
+  switch (AD5420_0_10V_RANGE)
+  {
+    case AD5420_0_5V_RANGE: {
+        //Vout=(V_RF_IN)*(Data/2^N)
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_10V_RANGE: {
+        //Vout=(V_RF_IN)*(Data/2^N)
+        data = (uint16_t)(65536 / 5) * ((*dac2_data - DS_Lower_Temp_Limit) /
+                         (DS_Upper_Temp_Limit - DS_Lower_Temp_Limit) * 10);
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_4_20I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_20I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_24I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+  } //End of switch
+
+
+  //Send NO OP to DAC1
+  SPI.transfer(AD5420_NOP);
+  SPI.transfer16(0x0000); //Send blank
+  ad5420_latch_trig();
+}
+
+void send_dac1_data(float *dac1_data) {
+  uint16_t data;
+
+  //Send NO OP to DAC2
+  SPI.transfer(AD5420_NOP);
+  SPI.transfer16(0x0000); //Send blank
+
+  //Send Data to DAC1
+  switch (AD5420_4_20I_RANGE)
+  {
+    case AD5420_0_5V_RANGE: {
+        //Vout=(V_RF_IN)*(Data/2^N)
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_10V_RANGE: {
+        //Vout=(V_RF_IN)*(Data/2^N)
+        data = (uint16_t)(65536 / 5) * ((*dac2_data - DS_Lower_Temp_Limit) /
+                         (DS_Upper_Temp_Limit - DS_Lower_Temp_Limit) * 10);
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_4_20I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_20I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+    case AD5420_0_24I_RANGE: {
+        SPI.transfer(AD5420_DATA_REG_ADDR);
+        SPI.transfer16(data);
+        break;
+      }
+  } //End of switch
+
+  ad5420_latch_trig(); //Trigger the latch
+}
+
+/***********************************************************
+**                                                        **
+**      Definitions for MUX                               **
+**                                                        **
+***********************************************************/
+
+//MUX Pins A and B
+#define Mux_A  9
+#define Mux_B  8
+
+/***********************************************************
+**                                                        **
+**      MUX Functions                                     **
+**                                                        **
+***********************************************************/
+//Setup the Mux pins and set it as LOW
+void mux_init() {
+  pinMode(Mux_A, OUTPUT);
+  pinMode(Mux_B, OUTPUT);
+  digitalWrite(Mux_A, LOW);
+  digitalWrite(Mux_B, LOW);
+}
+
+void mux_lcd() {
+  digitalWrite(Mux_A, LOW);
+  digitalWrite(Mux_B, LOW);
+}
+
+void mux_423() {
+  digitalWrite(Mux_A, HIGH);
+  digitalWrite(Mux_B, LOW);
+}
+
+
+/***********************************************************
+**                                                        **
+**      Main Code                                         **
+**                                                        **
+***********************************************************/
+
+void setup() {
+  //Setup Serial Port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Calibration");
 
-  // Start up the library
-  sensors.begin();
+  //Start SPI Module
+  SPI.begin();
 
-  //Make sure it finds all the devices and the addresses matches
+  //Initialize DAC
+  ad5420_init();
 
-  // locate devices on the bus
-  Serial.print("Locating devices...");
-  Serial.print("Found ");
-  Serial.print(sensors.getDeviceCount(), DEC);
-  Serial.println(" devices.");
-
-  // method 1: by index
-  if (!sensors.getAddress(DeviceA, 0)) Serial.println("Unable to find address for Device 0");
-  if (!sensors.getAddress(DeviceB, 1)) Serial.println("Unable to find address for Device 1");
-  if (!sensors.getAddress(DeviceC, 2)) Serial.println("Unable to find address for Device 2");
-  if (!sensors.getAddress(DeviceD, 3)) Serial.println("Unable to find address for Device 3");
-
-  // show the addresses we found on the bus
-  Serial.print("Device 0 Address: ");
-  printAddress(DeviceA);
-  Serial.println();
-
-  Serial.print("Device 1 Address: ");
-  printAddress(DeviceB);
-  Serial.println();
-
-  Serial.print("Device 2 Address: ");
-  printAddress(DeviceC);
-  Serial.println();
-
-  Serial.print("Device 3 Address: ");
-  printAddress(DeviceD);
-  Serial.println();
-
-  // set the resolution to 12 bit
-  sensors.setResolution(DeviceA, TEMPERATURE_PRECISION);
-  sensors.setResolution(DeviceB, TEMPERATURE_PRECISION);
-  sensors.setResolution(DeviceC, TEMPERATURE_PRECISION);
-  sensors.setResolution(DeviceD, TEMPERATURE_PRECISION);
-
-  Serial.print("Device 0 Resolution: ");
-  Serial.print(sensors.getResolution(DeviceA), DEC);
-  Serial.println();
-
-  Serial.print("Device 1 Resolution: ");
-  Serial.print(sensors.getResolution(DeviceB), DEC);
-  Serial.println();
-
-  Serial.print("Device 2 Resolution: ");
-  Serial.print(sensors.getResolution(DeviceC), DEC);
-  Serial.println();
-
-  Serial.print("Device 3 Resolution: ");
-  Serial.print(sensors.getResolution(DeviceD), DEC);
-  Serial.println();
+  //  uint16_t data = 0xFFFF;
+  //  send_dac_data(&data,&data);
 }
 
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // zero pad the address if necessary
-    if (deviceAddress[i] < 16) Serial.print("0");
-    Serial.print(deviceAddress[i], HEX);
-  }
-}
 
-// function to print a device's resolution
-void printResolution(DeviceAddress deviceAddress)
-{
-  Serial.print("Resolution: ");
-  Serial.print(sensors.getResolution(deviceAddress));
-  Serial.println();
-}
+void loop() {
+  // put your main code here, to run repeatedly:
 
-void loop(void)
-{
-  // call sensors.requestTemperatures() to issue a global temperature
-  // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
-
-  float DevA[readings], DevB[readings], DevC[readings], DevD[readings],
-        DevAT = 0, DevBT = 0, DevCT = 0, DevDT = 0;
-
-  double AvgA = 0, AvgB = 0, AvgC = 0, AvgD = 0;
-
-  for (int index = 0; index < readings; index++)
-  {
-    sensors.requestTemperatures();
-    DevA [index] = sensors.getTempC(DeviceA);
-    DevB [index] = sensors.getTempC(DeviceB);
-    DevC [index] = sensors.getTempC(DeviceC);
-    DevD [index] = sensors.getTempC(DeviceD);
-
-    DevAT += DevA[index];
-    DevBT += DevB[index];
-    DevCT += DevC[index];
-    DevDT += DevD[index];
-
-    delay(3000);
-  }
-  Serial.println("DONE");
-
-  AvgA = (DevAT / readings);
-  AvgB = (DevBT / readings);
-  AvgC = (DevCT / readings);
-  AvgD = (DevDT / readings);
-
-  // print the device information
-  Serial.print("DeviceA: ");
-  Serial.print(AvgA);
-  Serial.println(" C");
-
-  Serial.print("DeviceB: ");
-  Serial.print(AvgB);
-  Serial.println(" C");
-
-  Serial.print("DeviceC: ");
-  Serial.print(AvgC);
-  Serial.println(" C");
-
-  Serial.print("DeviceD: ");
-  Serial.print(AvgD);
-  Serial.println(" C");
 }
--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-GLHP_Trailer
-============
+#GLHP_Trailer
 
-Libraries needed:
+##Libraries needed:
 
-OneWire.h: From Arduino Database
-SPI.h: From Arduino Database
-DallasTemperature.h: From Arduino Database
-AD5422: See DAC references
+*OneWire.h: From Arduino Database
+*SPI.h: From Arduino Database
+*DallasTemperature.h: From Arduino Database
+*AD5422: See DAC references
+
+##For DS18B20 Calibration
+*See commit: 5575ebc0df84a17fe0933561ec45dda765844924
 
 
-References
+##References
 
-AD5422:
-https://github.com/analogdevicesinc/no-OS/tree/master/device_drivers/AD5421
-https://github.com/cyceron/TML <-See AD5422 .c/.h files
-https://github.com/d-wsky/DAC-AD5420 <- AVR example
-   
-DallasTemperature.h: http://www.milesburton.com/?title=Dallas_Temperature_Control_Library
+###AD5422:
+*https://github.com/analogdevicesinc/no-OS/tree/master/device_drivers/AD5421
+*https://github.com/cyceron/TML <-See AD5422 .c/.h files
+*https://github.com/d-wsky/DAC-AD5420 <- AVR example
+
+###DS18B20:   
+*DallasTemperature.h: http://www.milesburton.com/?title=Dallas_Temperature_Control_Library
